0f37e7862b7f15e4bc4eae45fb00a99d742a3415
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2022-04-26 16:34:32 +0530

    cmt

---
+++ counter/src/components/Counter.jsx
@@ -0,0 +1,38 @@
+import React, {useState} from 'react'
+
+const Counter = () => {
+
+    let [count, setCount] = useState(0);
+
+    const handleChange = (e)=>{
+      setCount(e.target.value)
+    }
+    const IncNum = () => {
+      setCount(count + 1);
+    };
+  
+    const DncNum = () => {
+      setCount(count - 1);
+    };
+  
+  
+    return (
+     <>
+        <h1 className="heading"></h1>
+  
+        <div className="container">
+          <button className="btn" onClick={DncNum}>
+            -
+          </button>
+          <input className="heading1" value={count} onChange={handleChange}/>
+          <button className="btn1" onClick={IncNum}>
+            +
+          </button>
+        </div>
+       
+      </>
+    );
+  
+}
+
+export default Counter;
--- counter/src/components/CounterComp.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const counterComp = () => {
-  return (
-  
-  )
-}
-
-export default counterComp
