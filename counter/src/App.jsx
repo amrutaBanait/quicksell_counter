79a219bc89cb363340f89b12493bfc7834fb5808
Amruta Banait <93419962+amrutaBanait@users.noreply.github.com>
2022-04-26 16:35:16 +0530

    cmt

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,40 +1,15 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import "./App.css";
-
+import Counter from "./components/Counter";
 
 const App = () => {
  
-  let [count, setCount] = useState(0);
-
-  const handleChange = (e)=>{
-    setCount(e.target.value)
-  }
-  const IncNum = () => {
-    setCount(count + 1);
-  };
-
-  const DncNum = () => {
-    setCount(count - 1);
-  };
-
-
   return (
-   <>
-      <h1 className="heading"></h1>
-
-      <div className="container">
-        <button className="btn" onClick={DncNum}>
-          -
-        </button>
-        <input className="heading1" value={count} onChange={handleChange}/>
-        <button className="btn1" onClick={IncNum}>
-          +
-        </button>
-      </div>
-     
+    <>
+    <Counter/>
     </>
-  );
+  )
 };
 
 export default App;
